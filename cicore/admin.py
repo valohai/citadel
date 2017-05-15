330870be10ee08191c32655180f62d0f4daa40c3
Aarni Koskela <akx@iki.fi>
2017-05-15 16:10:08 +0300

    Add Editor URL into admin

--- cicore/admin.py
+++ cicore/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.forms.utils import flatatt
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from cicore.models import Asset, Entry, Event, Round
 
@@ -15,12 +18,24 @@ class RoundAdmin(admin.ModelAdmin):
         'name',
         'is_visible',
         'accepting_entries',
+        'editor_url',
     )
     list_filter = (
         'event',
         'is_visible',
         'accepting_entries',
     )
+
+    def editor_url(self, instance):
+        url = reverse('round-editor', kwargs={'slug': instance.slug})
+        attrs = {
+            'href': url,
+            'target': '_blank',
+        }
+        return mark_safe('<a{}>{}</a>'.format(
+            flatatt(attrs),
+            url,
+        ))
 
 
 class EntryAdmin(admin.ModelAdmin):
