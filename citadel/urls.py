664702425663f500d2f75b43c7ebb311c6ff45d6
Aarni Koskela <akx@iki.fi>
2017-05-15 14:24:49 +0300

    Add 404.html

--- citadel/urls.py
+++ citadel/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('cifront.urls')),
+    url(r'^404.html$', TemplateView.as_view(template_name='404.html')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
