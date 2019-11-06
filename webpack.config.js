b84dc1553bcdd3628a0c155ea4335a5501978012
Aarni Koskela <akx@iki.fi>
2019-11-06 20:59:35 +0200

    Upgrade JS build toolchain

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,26 @@
-const autoprefixer = require('autoprefixer');
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './cifront/app/scripts/app.coffee',
+  entry: "./cifront/app/scripts/app.coffee",
   output: {
-    path: path.resolve('./cifront/static/editor'),
-    filename: 'bundle.js',
+    path: path.resolve("./cifront/static/editor"),
+    filename: "bundle.js"
   },
   resolve: {
     extensions: [".js", ".coffee", ".scss", ".css", ".ttf"],
     alias: {
-      "assets": path.resolve('./cifront/app/assets'),
-    },
+      assets: path.resolve("./cifront/app/assets")
+    }
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: ((loader) => [
-                autoprefixer({browsers: ["last 2 version"]}),
-              ])
-            },
-          },
-          'sass-loader',
-        ]
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
       },
-      {test: /\.coffee$/, use: ['coffee-loader']},
-      {test: /\.png/, use: ['url-loader?mimetype=image/png']},
-      {test: /\.ttf/, use: ['url-loader?mimetype=font/ttf']},
-    ],
-  },
+      { test: /\.coffee$/, use: ["coffee-loader"] },
+      { test: /\.png/, use: ["url-loader?mimetype=image/png"] },
+      { test: /\.ttf/, use: ["url-loader?mimetype=font/ttf"] }
+    ]
+  }
 };
